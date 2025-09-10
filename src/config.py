5d681e64317ff4945a91d3ab7f8fa9aea2aa6c8c
Olga Noskova <olga.tolba@gmail.com>
2025-09-11 09:07:57 +1000

    feat(config): add function to load database connection settings

---
+++ src/config.py
@@ -0,0 +1,17 @@
+from configparser import ConfigParser
+
+
+def config(filename="database.ini", section="postgresql") -> dict:
+    """Читает параметры подключения к базе данных из файла database.ini"""
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(f"Section {section} is not found in {filename} file.")
+
+    return db
